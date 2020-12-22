2798da84319c2773e64f35e4ea421b3ac15ffc81
Jonathan Oliveira <John.deoliveira@outlook.com>
2020-12-22 10:29:59 -0300

    Projeto Criado

---
+++ src/test/java/dominio/empresa/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package dominio.empresa.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
