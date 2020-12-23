11f41009653c11d3432a38f3494a69086df68225
Jonathan Oliveira <John.deoliveira@outlook.com>
2020-12-22 22:52:13 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/dominio/empresa/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package dominio.empresa.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dominio.empresa.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
